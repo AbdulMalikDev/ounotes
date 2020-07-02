9a9c33234ebd3f915d90e100ea00ef2821948e6a
Syed Wajid <syedwajid3399@gmail.com>
2020-07-02 21:39:42 +0530

    a

--- lib/ui/views/admin/report/report_view.dart
+++ lib/ui/views/admin/report/report_view.dart
@@ -3,11 +3,19 @@ import 'package:FSOUNotes/ui/views/admin/report/report_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
-class ReportView extends StatelessWidget {
+class ReportView extends StatefulWidget {
   const ReportView({Key key}) : super(key: key);
 
   @override
+  _ReportViewState createState() => _ReportViewState();
+}
+
+class _ReportViewState extends State<ReportView>
+    with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    super.build(context);
     return ViewModelBuilder<ReportViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         body: Container(
@@ -35,18 +43,33 @@ class ReportView extends StatelessWidget {
                             margin: EdgeInsets.symmetric(
                                 horizontal: 20, vertical: 20),
                             child: Card(
+                              color: Theme.of(context).colorScheme.background,
                               elevation: 10,
-                              
                               child: ListTile(
                                 contentPadding: EdgeInsets.all(15),
                                 title: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
-                                    Text("FileType : " + report.type),
-                                    Text("Subject Name :" + report.subjectName),
-                                    Text("Email of Reporter : " + report.email),
-                                    Text("Reports : " +
-                                        report.reports.toString()),
+                                    Text(
+                                      "FileType : " + report.type,
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(
+                                      "Subject Name :" + report.subjectName,
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(
+                                      "Email of Reporter : " + report.email,
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(
+                                      "Reports : " + report.reports.toString(),
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
                                   ],
                                 ),
                                 onTap: () async {
@@ -65,4 +88,7 @@ class ReportView extends StatelessWidget {
       viewModelBuilder: () => ReportViewModel(),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
--- lib/ui/views/admin/upload_log/upload_log_view.dart
+++ lib/ui/views/admin/upload_log/upload_log_view.dart
@@ -3,16 +3,24 @@ import 'package:FSOUNotes/ui/views/admin/upload_log/upload_log_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
-class UploadLogView extends StatelessWidget {
+class UploadLogView extends StatefulWidget {
   const UploadLogView({Key key}) : super(key: key);
 
   @override
+  _UploadLogViewState createState() => _UploadLogViewState();
+}
+
+class _UploadLogViewState extends State<UploadLogView>
+    with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
+    var theme = Theme.of(context);
     return ViewModelBuilder<UploadLogViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         body: Container(
           //color: Colors.yellow,
-          height: MediaQuery.of(context).size.height * 0.85,
+          height: MediaQuery.of(context).size.height * 0.8,
           // alignment: Alignment.center,
           child: Center(
             child: Column(
@@ -32,20 +40,45 @@ class UploadLogView extends StatelessWidget {
                           UploadLog logItem = model.logs[index];
                           return Container(
                             margin: EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 20),
+                                horizontal: 20, vertical: 10),
                             child: Card(
+                              color: Theme.of(context).colorScheme.background,
                               elevation: 10,
                               child: ListTile(
                                 contentPadding: EdgeInsets.all(15),
                                 title: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
-                                    Text("File Name : "+ logItem.fileName),
-                                    Text("FileType : " + logItem.type),
-                                    Text("Subject Name :" + logItem.subjectName),
-                                    Text("Email of Uploader : " + logItem.email),
-                                    Text("UploadedAt : " + logItem.date.toString()),
-                                    Text("Size : " + logItem.size ?? "0"),
+                                    Text(
+                                      "File Name : " + logItem.fileName,
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(
+                                      "FileType : " + logItem.type,
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(
+                                      "Subject Name :" + logItem.subjectName,
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(
+                                      "Email of Uploader : " + logItem.email,
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(
+                                      "UploadedAt : " + logItem.date.toString(),
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(
+                                      "Size : " + logItem.size ?? "0",
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
                                   ],
                                 ),
                                 onTap: () async {
@@ -64,4 +97,7 @@ class UploadLogView extends StatelessWidget {
       viewModelBuilder: () => UploadLogViewModel(),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
