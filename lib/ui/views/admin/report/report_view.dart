71d7769925e4a22eebf2280e7bdf61227f335576
unknown <mam7264@gmail.com>
2020-07-03 10:17:00 +0530

    date added in reports view

--- lib/ui/views/admin/report/report_view.dart
+++ lib/ui/views/admin/report/report_view.dart
@@ -53,22 +53,37 @@ class _ReportViewState extends State<ReportView>
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
                                     Text(
+                                      "File Name : " + report.title,
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(""),
+                                    Text(
                                       "FileType : " + report.type,
                                       style: theme.textTheme.subtitle1
                                           .copyWith(fontSize: 18),
                                     ),
+                                    Text(""),
                                     Text(
                                       "Subject Name :" + report.subjectName,
                                       style: theme.textTheme.subtitle1
                                           .copyWith(fontSize: 18),
                                     ),
+                                    Text(""),
                                     Text(
                                       "Email of Reporter : " + report.email,
                                       style: theme.textTheme.subtitle1
                                           .copyWith(fontSize: 18),
                                     ),
+                                    Text(""),
                                     Text(
                                       "Reports : " + report.reports.toString(),
+                                      style: theme.textTheme.subtitle1
+                                          .copyWith(fontSize: 18),
+                                    ),
+                                    Text(""),
+                                    Text(
+                                      "Date : " + report.date.toString(),
                                       style: theme.textTheme.subtitle1
                                           .copyWith(fontSize: 18),
                                     ),
