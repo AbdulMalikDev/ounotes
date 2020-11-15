9d8f4882eff321c8f3b236177b46fd7c4e2fdd2f
Abdul Malik <160418733095@mjcollege.ac.in>
2020-11-15 17:54:48 +0530

    Web view bug fixed

--- lib/ui/views/web_view/web_view.dart
+++ lib/ui/views/web_view/web_view.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:FSOUNotes/ui/views/web_view/web_view_viewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:stacked/stacked.dart';
 // import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewWidget extends StatefulWidget {
@@ -13,34 +16,88 @@ class WebViewWidget extends StatefulWidget {
 }
 
 class _WebViewWidgetState extends State<WebViewWidget> {
+  // Instance of WebView plugin
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+
+  // On destroy stream
+  StreamSubscription _onDestroy;
+
+  // On urlChanged stream
+  StreamSubscription<String> _onUrlChanged;
+
+  final _history = [];
+
+  WebViewModel localModel;
+
   @override
   void initState() {
     super.initState();
-    // Enable hybrid composition.
-    // if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    flutterWebViewPlugin.close();
+
+    // Add a listener to on url changed
+    _onUrlChanged = flutterWebViewPlugin.onUrlChanged.listen((String url) {
+      if (mounted) {
+        print("!!!!!!!!!!!!!!!!!!!!");
+        print(url);
+        print(url.contains("export=download"));
+        print(localModel);
+        if ( ( url.contains("export=download") || url.contains("docs.googleusercontent.com/") ) && localModel != null) {
+          print("showDialog");
+          localModel.showDownloadPreventDialog(flutterWebViewPlugin);
+        }
+        setState(() {
+          _history.add('onUrlChanged: $url');
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    flutterWebViewPlugin.dispose();
+    _onUrlChanged.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return new WebviewScaffold(
-      initialChild: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Loading ....",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.teal,fontSize: 25),),
-            SizedBox(height: 40,),
-            CircularProgressIndicator(),
-          ],
-        ),
-      ),
-      hidden: true,
-      url: widget.url,
-      appBar: new AppBar(
-        iconTheme: IconThemeData(
-          color: Colors.white, //change your color here
-        ),
-        title: new Text("Notes"),
-      ),
-    );
+    //* Since The app bar is a bit annoying when in landscape
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    return ViewModelBuilder.reactive(
+        onModelReady: (model) => localModel = model,
+        builder: (context, model, child) {
+          return new WebviewScaffold(
+            initialChild: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Loading ....",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.teal,
+                        fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  CircularProgressIndicator(),
+                ],
+              ),
+            ),
+            hidden: true,
+            url: widget.url,
+            appBar: isLandscape
+                ? null
+                : new AppBar(
+                    iconTheme: IconThemeData(
+                      color: Colors.white, //change your color here
+                    ),
+                    title: new Text("Notes"),
+                  ),
+          );
+        },
+        viewModelBuilder: () => WebViewModel());
   }
 }
---
+++ lib/ui/views/web_view/web_view_viewModel.dart
@@ -0,0 +1,16 @@
+import 'package:FSOUNotes/app/locator.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class WebViewModel extends BaseViewModel{
+  DialogService _dialogService = locator<DialogService>();
+  NavigationService _navigationService = locator<NavigationService>();
+  void showDownloadPreventDialog(FlutterWebviewPlugin web)async {
+    await web.close();
+    await _dialogService.showDialog(title: "WARNING" , description: "Notes cannot be downloaded.");
+    _navigationService.popRepeated(1);
+  }
+
+
+}
